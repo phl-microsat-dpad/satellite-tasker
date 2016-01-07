93b1542654a24b3719455d1e353654aae7d8ed23
Benjie Jiao <hi@benjie.me>
2016-01-08 01:47:48 +0800

    Using script_root variable instead of jsglue

--- imagery_request/__init__.py
+++ imagery_request/__init__.py
@@ -1,12 +1,10 @@
 from flask import Flask
 from flask_restful import Api
-from flask_jsglue import JSGlue
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
 
-jsglue = JSGlue(app)
 db = SQLAlchemy(app)
 
 from imagery_request import views, models, endpoints
