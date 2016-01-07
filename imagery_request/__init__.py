b191e1d58e8f0078d5e468771127fdd710411152
Benjie Jiao <hi@benjie.me>
2016-01-08 02:02:22 +0800

    Added logger

--- imagery_request/__init__.py
+++ imagery_request/__init__.py
@@ -1,10 +1,18 @@
+import logging
 from flask import Flask
 from flask_restful import Api
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.debug=True
+app.debug = True
 app.config.from_object('config')
+
+# Setup Logging
+formatter = logging.Formatter(app.config.get('LOG_FORMAT'))
+fh = logging.FileHandler(filename=app.config.get('LOG_FILE'))
+fh.setFormatter(formatter)
+fh.setLevel(logging.DEBUG)
+app.logger.addHandler(fh)
 
 db = SQLAlchemy(app)
 
